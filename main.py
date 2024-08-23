fb4504b1611772db7ed174a32af4954d635d689f
markov-ngz <123246412+markov-ngz@users.noreply.github.com>
2024-08-23 21:40:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,30 @@ import os
 from GUI import GUI
 from ManhuaTranslator import ManhuaTranslator
 
-MODEL_PATH = "../models/sam_model/"
-PROCESSOR_PATH = "../models/sam_processor/"
-TRANSLATOR_NAME = "Helsinki-NLP/opus-mt-ko-en"
+# Resources 
+MODEL_PATH = os.getenv("SAM_PATH")
+PROCESSOR_PATH =  os.getenv("SAM_PROCESSOR")
+TRANSLATOR_NAME =  os.getenv("TRANSLATOR_NAME")
 
-
+# Image's path
+IMAGE_PATH =  os.getenv("SAM_PATH")
+RESULT_PATH = os.getenv("SAM_PATH")
 
 def main()->None:
-    
-    path = "image.png"
-    save_path = "result.png"
-    
-    manhua_translator = ManhuaTranslator(MODEL_PATH,PROCESSOR_PATH,TRANSLATOR_NAME)
-    gui = GUI()
-    image_PIL , input_boxes ,input_points = gui.get_inputs(path)
 
-    if image_PIL == None and input_boxes == None and input_points == None : 
+    # 1. Load models 
+    manhua_translator = ManhuaTranslator(MODEL_PATH,PROCESSOR_PATH,TRANSLATOR_NAME)
+
+    # 2. Get text location input 
+    gui = GUI()
+    image_PIL , input_boxes ,input_points = gui.get_inputs(IMAGE_PATH)
+
+    if image_PIL == None and input_boxes == None and input_points == None : # Exit if nothing was chosen 
         sys.exit(0)
 
+    # 3. Extract text , inpaint on the image and save result 
     manhua_translator.manhua_translator(image_PIL,
-                                        save_path,
+                                        RESULT_PATH,
                                         input_boxes,
                                         input_points)
     
